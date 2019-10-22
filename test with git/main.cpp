71e4fa5e4b115f76aac428383baab1a5bafd7c8d
TC <tc@tcs-macbook-pro.local>
2019-10-21 20:27:57 -0700

    Initial Commit

---
+++ test with git/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test with git
+//
+//  Created by TC on 10/21/19.
+//  Copyright © 2019 TC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
